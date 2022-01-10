861e41bbda017e540b53083cf88e4e6774b538db
Wes Bonelli <wpb3hw@virginia.edu>
2022-01-09 22:04:32 -0600

    color analysis

--- pytcher_plants/color.py
+++ pytcher_plants/color.py
@@ -28,6 +28,7 @@ def rgb_analysis(treatment, output_directory, subset):
     plt.legend().remove()
     plt.title(f"{treatment} color distribution")
     plt.savefig(join(output_directory, f"{treatment}.k{k}.dist.png"))
+    plt.show()
     plt.clf()
 
     fig = go.Figure()
@@ -39,23 +40,25 @@ def rgb_analysis(treatment, output_directory, subset):
     trace=dict(type='scatter3d', x=r, y=g, z=b, mode='markers', marker=dict(color=colors_map, size=sizes_map))
     fig.add_trace(trace)
     fig.update_layout(title=treatment, scene=dict(xaxis_title='G', yaxis_title='R', zaxis_title='B'))
+    fig.show()
     fig.write_image(join(output_directory, treatment + '.rgb.3d.png'))
 
 
 def hsv_analysis(treatment, output_directory, subset):
-    ranges = {((k * 5) + 5):list(range(k * 5, (k * 5) + 5)) for k in range(0, 72)}
-    ranges_round = {min(v):k for k, v in ranges.items()}
+    # (72 equally spaced from 5 to 355)
+    divisor = 5
+    ranges = [((k * divisor) + divisor) for k in range(0, int(360 / divisor))]
 
-    # format HSV columns, convert to [1, 360] range, create hue bands (72 equally spaced from 5 to 355)
+    # format HSV columns, convert to [1, 360] range, create hue bands
     subset_hsv = subset[['H', 'S', 'V']].astype(float)
     subset_hsv['HH'] = subset_hsv.apply(lambda row: int(float(row['H']) * 360), axis=1)
-    subset_hsv['Band'] = subset_hsv.apply(lambda row: ranges_round[round(int(row['HH']), -1)], axis=1)
+    subset_hsv['Band'] = subset_hsv.apply(lambda row: int(row['HH']) - (int(row['HH']) % divisor), axis=1)
 
     # count clusters per band
     counts = Counter(subset_hsv['Band'])
     counts_keys = list(counts.keys())
-    for key in [k for k in ranges.keys() if k not in counts_keys]: counts[key] = 0  # pad zeroes
-    for key in [k for k in ranges.keys() if 125 < k < 360]: counts[key] = 0         # remove outliers (non red/green)
+    for key in [k for k in ranges if k not in counts_keys]: counts[key] = 0  # pad zeroes
+    for key in [k for k in ranges if 125 < k < 360]: counts[key] = 0         # remove outliers (non red/green)
     total = sum(counts.values())
     mass = OrderedDict(sorted({k:float(v / total) for k, v in counts.items()}.items()))
     mass_df = pd.DataFrame(zip([str(k) for k in mass.keys()], mass.values()), columns=['band', 'mass'])
@@ -65,11 +68,12 @@ def hsv_analysis(treatment, output_directory, subset):
         mass_df,
         title=f"Hue distribution ({treatment})",
         r='mass',
-        range_r=[0, 0.5], # max(mass_df['mass'])
+        range_r=[0, max(mass_df['mass'])],
         theta='band',
         range_theta=[0,360],
         color='band',
         color_discrete_map={str(k): hue_to_rgb_formatted(k) for k in counts.keys()},
         labels=None)
     fig.update_layout(showlegend=False, polar_angularaxis_tickfont_size=7, polar_radialaxis_tickfont_size=7)
+    fig.show()
     fig.write_image(join(output_directory, treatment + '.hue.radial.png'))
