fdb46cb386b78d274a434b14fe3ff623c5b22076
Wes Bonelli <wpb3hw@virginia.edu>
2022-06-09 11:44:22 -0400

    reorganize samples, refactoring, fix tests, update readme

--- pytcherplants/tests/test_cli.py
+++ pytcherplants/tests/test_cli.py
@@ -7,46 +7,53 @@ from click.testing import CliRunner
 import pytcherplants.cli as cli
 
 group_path = 'samples/groups/1_14_19.10_30_20.5V4B3121.jpg'
-plant_path = 'samples/plants/1_14_19.10_30_20.?.jpg'
-masked_path = 'samples/masks/1_14_19.10_30_20.5V4B3121.masked.jpg'
+group_masked_path = 'samples/groups/1_14_19.10_30_20.5V4B3121.masked.jpg'
+plant_path = 'samples/plants/1_14_19.10_30_20.p001.jpg'
+plant_masked_path = 'samples/plants/1_14_19.10_30_20.p001.masked.jpg'
 
 
-def test_color_analysis_count_0():
+def test_classify():
     with TemporaryDirectory() as output_path:
-        input_stem = Path(masked_path).stem
+        input_stem = Path(plant_masked_path).stem
         runner = CliRunner()
-        runner.invoke(cli.color_analysis, ['-i', masked_path, '-o', output_path])
+        runner.invoke(cli.classify, ['-i', group_path, '-o', output_path])
+
+        results = listdir(output_path)
+        assert len(results) == 1
+        assert f"{input_stem}.masked.jpg" in results
+
+
+def test_segment_count_1():
+    with TemporaryDirectory() as output_path:
+        input_stem = Path(plant_masked_path).stem
+        runner = CliRunner()
+        runner.invoke(cli.segment, ['-i', plant_masked_path, '-o', output_path])
+
+        results = listdir(output_path)
+        assert len(results) == 2
+        assert f"{input_stem}.plants.png" in results
+        assert f"{input_stem}.plant.1.png" in results
+
+
+def test_segment_count_2():
+    with TemporaryDirectory() as output_path:
+        input_stem = Path(plant_masked_path).stem
+        runner = CliRunner()
+        runner.invoke(cli.segment, ['-i', plant_masked_path, '-o', output_path, '-c', 2])
+
+        results = listdir(output_path)
+        assert len(results) == 3
+        assert f"{input_stem}.plants.png" in results
+        assert f"{input_stem}.plant.1.png" in results
+        assert f"{input_stem}.plant.2.png" in results
+
+
+def test_analyze():
+    with TemporaryDirectory() as output_path:
+        input_stem = Path(plant_masked_path).stem
+        runner = CliRunner()
+        runner.invoke(cli.analyze, ['-i', plant_masked_path, '-o', output_path])
 
         results = listdir(output_path)
         assert len(results) == 1
         assert f"{input_stem}.colors.csv" in results
-
-
-def test_color_analysis_count_1():
-    with TemporaryDirectory() as output_path:
-        input_stem = Path(masked_path).stem
-        runner = CliRunner()
-        runner.invoke(cli.color_analysis, ['-i', masked_path, '-o', output_path, '-c', 1])
-
-        results = listdir(output_path)
-        assert len(results) == 4
-        assert f"{input_stem}.plants.png" in results
-        assert f"{input_stem}.plants.csv" in results
-        assert f"{input_stem}.1.colors.csv" in results
-        assert f"{input_stem}.1.png" in results
-
-
-def test_color_analysis_count_2():
-    with TemporaryDirectory() as output_path:
-        input_stem = Path(masked_path).stem
-        runner = CliRunner()
-        runner.invoke(cli.analyze, ['-i', masked_path, '-o', output_path, '-c', 2])
-
-        results = listdir(output_path)
-        assert len(results) == 6
-        assert f"{input_stem}.plants.png" in results
-        assert f"{input_stem}.plants.csv" in results
-        assert f"{input_stem}.1.colors.csv" in results
-        assert f"{input_stem}.1.png" in results
-        assert f"{input_stem}.2.colors.csv" in results
-        assert f"{input_stem}.2.png" in results
--- pytcherplants/tests/test_clustering.py
+++ pytcherplants/tests/test_clustering.py
@@ -2,11 +2,11 @@ import cv2
 
 from pytcherplants.clustering import get_clusters
 
-input_path = 'samples/masks/1_14_19.10_30_20.5V4B3121.masked.jpg'
+plant_path = 'samples/groups/1_14_19.10_30_20.p001.masked.jpg'
 
 
 def test_get_clusters_default_k_and_no_filters():
-    image = cv2.imread(input_path)
+    image = cv2.imread(plant_path)
     counts = get_clusters(image)
     assert len(counts.keys()) == 14
     assert all(v > 0 for v in counts.values())
@@ -14,7 +14,7 @@ def test_get_clusters_default_k_and_no_filters():
 
 
 def test_get_clusters_default_k_and_filters():
-    image = cv2.imread(input_path)
+    image = cv2.imread(plant_path)
     counts = get_clusters(image, filters=[((0, 38, 40), (39, 255, 255))])  # filter out blues/purples
     assert len(counts.keys()) == 14
     assert all(v > 0 for v in counts.values())
@@ -22,7 +22,7 @@ def test_get_clusters_default_k_and_filters():
 
 
 def test_get_clusters_custom_k():
-    image = cv2.imread(input_path)
+    image = cv2.imread(plant_path)
     counts = get_clusters(image, k=10)
     assert len(counts.keys()) == 9
     assert all(v > 0 for v in counts.values())
